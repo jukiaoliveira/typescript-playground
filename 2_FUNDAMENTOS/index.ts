92993001a4b3d76ae8698707f46d3dc7c108d478
jukiaoliveira <jurafaoliveira@icloud.com>
2025-10-28 20:41:43 -0300

    feat: add exemplos sobre o tipo number

--- 2_FUNDAMENTOS/index.ts
+++ 2_FUNDAMENTOS/index.ts
@@ -1,2 +1,16 @@
 // 1 - numbers
 let x: number = 10
+
+console.log(x)
+
+x = 16
+
+console.log(typeof x)
+
+const y:number = 15.584848
+
+console.log(typeof y)
+
+console.log(y)
+
+console.log(y.toPrecision(3))
