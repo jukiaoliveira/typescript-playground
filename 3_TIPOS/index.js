c0612b2b8bdfd6b775afc2af3e35a3c7e2bd6873
jukiaoliveira <jurafaoliveira@icloud.com>
2025-10-28 21:39:18 -0300

    feat: add exemplo de array

---
+++ 3_TIPOS/index.js
@@ -0,0 +1,9 @@
+// 1 - arrays
+let numbers = [1, 2, 3];
+numbers.push(5);
+console.log(numbers[2]);
+// numbers = "teste"
+const nomes = ["Julia", "Vitória"];
+export {};
+// nomes.push(3)
+//# sourceMappingURL=index.js.map
---
+++ 3_TIPOS/index.ts
@@ -0,0 +1,12 @@
+// 1 - arrays
+let numbers: number[] = [1, 2, 3]
+
+numbers.push(5)
+
+console.log(numbers[2])
+
+// numbers = "teste"
+
+const nomes: string[] = ["Julia", "Vitória"]
+
+// nomes.push(3)
