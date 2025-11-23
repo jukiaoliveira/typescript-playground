1af270d181e4b53c6ef81ba62d374613e6d8bf9e
jukiaoliveira <jurafaoliveira@icloud.com>
2025-11-23 18:23:15 -0300

    feat: add variaveis

--- 4_react_ts/src/App.tsx
+++ 4_react_ts/src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 function App() {
+
+  // 1 - variáveis
+  const name: string = "Júlia";
+  const age: number = 26;
+  const isWorking: boolean = true;
+
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && <p>Está estudando!</p>}
     </div>
   );
 }
