dfc2640c3bf632a9734b055f081ec988910d5f51
jukiaoliveira <jurafaoliveira@icloud.com>
2025-11-23 18:33:04 -0300

    feat: add funções em componentes

--- 4_react_ts/src/App.tsx
+++ 4_react_ts/src/App.tsx
@@ -7,12 +7,19 @@ function App() {
   const age: number = 26;
   const isWorking: boolean = true;
 
+  // 2 - funções
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}!`;
+  };
+
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
       <h2>Nome: {name}</h2>
       <p>Idade: {age}</p>
       {isWorking && <p>Está estudando!</p>}
+
+      <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
